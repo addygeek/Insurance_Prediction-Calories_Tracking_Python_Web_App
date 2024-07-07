a2b2a2f71747964d4f2e9a4e29503b5ac416ddb6
addygeek <ui22cs03@iiitsurat.ac.in>
2024-07-08 00:42:10 +0530

    Streal-Lit Input Fileds Expert

--- STEAMLAB BASICS/input-widgets.py
+++ STEAMLAB BASICS/input-widgets.py
@@ -23,5 +23,64 @@ with open("./image/india_won.jpeg","rb") as file:
         mime="image/jpeg"
     )
 
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
+df1 = pd.read_csv('./data/addresses.csv')
+csv = convert_df(df1)
 
+st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name='addresses.csv',
+    mime='text/csv'
+)
 
+st.subheader("Checkbox")
+agree = st.checkbox(
+    "I agree to T&C"
+)
+if agree:
+    st.write("Thanks to Agreeing our T and C!")
+
+st.subheader("Radio Button")
+
+qns = st.radio("What is your favorite Color",
+               ("Red","Blue","Green","Yellow"))
+if qns == "Red":
+    st.write("You selected Red")
+elif qns == "Blue":
+    st.write("You selected Blue")
+elif qns == "Green":
+    st.write("You selected Green")
+elif qns == "Yellow":
+    st.write("You selected Yellow")
+else:
+    st.write("Select a valid color")
+
+st.subheader("Select box")
+
+option = st.selectbox("What is your favorite Color",
+                      ("Red","Blue","Green","Yellow"))
+st.write("You selected",option)
+
+st.subheader("How Old are You?")
+age = st.slider("Select your age",1,100)
+st.write("I am ",age," years old")
+
+st.subheader("Text Input")
+
+name = st.text_input("Enter your name")
+st.write("Your name is ",name)
+
+st.subheader("Number Input")
+
+year_of_birth = st.number_input("Year of Birth")
+st.write("Your year is ",year_of_birth)
+
+import datetime
+
+dob = st.date_input(
+                    "Enter your Birthday",
+                    datetime.date(2000,1,1)
+)
+st.write("Your Date of birth is ",dob)
